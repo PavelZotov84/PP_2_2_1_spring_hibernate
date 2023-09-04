68ad4abbe600fd319a57c0d1b333678cecd0d310
PavelZotov84 <pavlozotov1984@yandex.ru>
2023-09-04 10:04:16 +0300

    my solution

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,34 +1,57 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.sql.SQLException;
-import java.util.List;
+import javax.persistence.NoResultException;
+
+
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
 
-      UserService userService = context.getBean(UserService.class);
+// создаём пользователей с машинами
+        User user1 = new User("Ivan", "Petrov", "Petrov@mail.ru");
+        User user2 = new User("Petr", "Ivanov", "Ivanov@mail.ru");
+        User user3 = new User("Oleg", "Udov", "Udov@mail.ru");
+        User user4 = new User("Maria", "Markova", "Markova@mail.ru");
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        Car car1 = new Car("Lada", 2109);
+        Car car2 = new Car("Reno", 1206);
+        Car car3 = new Car("Mazda", 5);
+        Car car4 = new Car("Kia", 187);
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+        // добавляем  пользователей с машинами в таблицу
+        userService.add(user1.setCar(car1).setUser(user1));
+        userService.add(user2.setCar(car2).setUser(user2));
+        userService.add(user3.setCar(car3).setUser(user3));
+        userService.add(user4.setCar(car4).setUser(user4));
 
-      context.close();
-   }
+        // выводим список пользователей из таблицы
+        System.out.println("1. Пользователи с машинами :");
+        for (User user : userService.listUsers()) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Model = " + user.getCar());
+            System.out.println();
+        }
+
+        //ищем и выводим пользователя по марке и серии авто
+        System.out.println("2. Поиск пользователя по модели и серии автомобиля :");
+        try {
+            System.out.println(userService.getUserByCar("Lada", 2109));
+        } catch (NoResultException e) {
+            System.out.println("нет пользователя с таким автомобилем");
+        }
+
+        context.close();
+    }
 }
